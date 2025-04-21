06529f7029c6114a3e78c8dbdb1a1d0b23270f5e
南浦月 <nanpuyue@gmail.com>
2025-04-21 08:23:10 +0000

    feat: support to specify listening address

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,70 @@
 use std::error::Error;
+use std::net::SocketAddr;
 use std::net::{Ipv4Addr, SocketAddrV4};
+use std::str::FromStr;
 
 use clap::Parser;
+use nix::sys::socket::SockaddrIn;
 use nix::sys::socket::sockopt::{Mark, OriginalDst};
 use nix::sys::socket::{getsockopt, setsockopt};
 use tokio::io::copy_bidirectional;
 use tokio::net::{TcpListener, TcpSocket};
 
 #[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
+#[command(version, about)]
 struct Args {
     #[arg(short, long)]
-    port: u16,
+    listen: Listen,
 
     #[arg(short, long)]
     mark: Option<u32>,
 }
 
+#[derive(Clone, Debug)]
+enum Listen {
+    Addr(SocketAddrV4),
+    Port(u16),
+}
+
+impl FromStr for Listen {
+    type Err = Box<dyn Error + Send + Sync>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(if s.contains(':') {
+            Self::Addr(s.parse()?)
+        } else {
+            Self::Port(s.parse()?)
+        })
+    }
+}
+
+fn error<E: Error + 'static>(e: E) {
+    println!("{e}");
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let port = args.port;
+    let listen: SocketAddr = match args.listen {
+        Listen::Addr(x) => x.into(),
+        Listen::Port(x) => (Ipv4Addr::LOCALHOST, x).into(),
+    };
     let mark = args.mark;
 
-    let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, port)).await?;
+    let listener = TcpListener::bind(listen).await?;
     loop {
         let (mut tcpstream, addr) = listener.accept().await?;
         tokio::spawn(async move {
-            let orig_dst = getsockopt(&tcpstream, OriginalDst).map_err(error)?;
-            let orig_dst = SocketAddrV4::new(
-                Ipv4Addr::from_bits(u32::from_be(orig_dst.sin_addr.s_addr)),
-                u16::from_be(orig_dst.sin_port),
-            );
-
+            let orig_dst = SockaddrIn::from(getsockopt(&tcpstream, OriginalDst).map_err(error)?);
             let tcpsocket = TcpSocket::new_v4().map_err(error)?;
             if let Some(x) = mark {
                 setsockopt(&tcpsocket, Mark, &x).map_err(error)?;
             }
-            let mut upstream = tcpsocket.connect(orig_dst.into()).await.map_err(error)?;
+
+            let mut upstream = tcpsocket
+                .connect((orig_dst.ip(), orig_dst.port()).into())
+                .await
+                .map_err(error)?;
             println!("{addr} -> {orig_dst} connected");
             copy_bidirectional(&mut tcpstream, &mut upstream)
                 .await
@@ -47,8 +74,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
             Ok::<_, ()>(())
         });
     }
-}
-
-fn error<E: Error + 'static>(e: E) {
-    println!("{e}");
 }
